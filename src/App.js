ce19ae54927921c4c07fe372200226c9357f94d9
Danny-dev23 <petrovslavik1978@gmail.com>
2025-07-09 20:50:25 +0600

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,37 @@ import "./App.css"; // Импорт глобальных стилей
 // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzUyNjc2Nzk0LCJpYXQiOjE3NTIwNzE5OTQsImp0aSI6ImQ2NGQ2NzU2M2JhZTRiZmE5ZjZlMzhmOGE3ZDM4ZDg4IiwidXNlcl9pZCI6NH0.MTL-7KqCYDpwUpvbSLiUx-PmKGbsbJID2KV_FQRDITs
 
 const App = () => {
-  useEffect(() => {
+useEffect(() => {
     sessionStorage.setItem(
       "access_token",
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzUyNjc2Nzk0LCJpYXQiOjE3NTIwNzE5OTQsImp0aSI6ImQ2NGQ2NzU2M2JhZTRiZmE5ZjZlMzhmOGE3ZDM4ZDg4IiwidXNlcl9pZCI6NH0.MTL-7KqCYDpwUpvbSLiUx-PmKGbsbJID2KV_FQRDITs"
     );
+    const user = {
+      active_balance: "0.00",
+      avatar: null,
+      city: "",
+      completed_orders: 0,
+      currency: "Ruble",
+      date_of_birth: null,
+      educational_institution: null,
+      email: "danilpetrov.1978@gmail.com",
+      expert_status: "beginner",
+      faculty: "",
+      frozen_balance: "0.00",
+      id: 4,
+      is_active: true,
+      is_online: false,
+      is_verified: false,
+      last_10_arbitrations: 0,
+      last_activity: "2025-07-09T17:39:49.797746+03:00",
+      positive_reviews: 0,
+      role: "customer",
+      specialization: "",
+      username: "danil"
+    };
+    sessionStorage.setItem("user", JSON.stringify(user));
+    sessionStorage.setItem("user_id", "4");
+    sessionStorage.setItem("role", "customer");
   }, []);
   return (
     <Router>
