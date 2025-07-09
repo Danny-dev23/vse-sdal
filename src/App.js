42e9fdaaa15d298a9d58f20a013364155d86c640
Danny-dev23 <petrovslavik1978@gmail.com>
2025-07-09 20:42:27 +0600

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import routes from "./router.js";
 import Footer from "./Components/Footer/Footer.jsx";
 import { TabsProvider } from "./utils/TabsContext.jsx";
 import "./App.css"; // Импорт глобальных стилей
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzUyNjc2Nzk0LCJpYXQiOjE3NTIwNzE5OTQsImp0aSI6ImQ2NGQ2NzU2M2JhZTRiZmE5ZjZlMzhmOGE3ZDM4ZDg4IiwidXNlcl9pZCI6NH0.MTL-7KqCYDpwUpvbSLiUx-PmKGbsbJID2KV_FQRDITs
 
 const App = () => {
+  useEffect(() => {
+    sessionStorage.setItem(
+      "access_token",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzUyNjc2Nzk0LCJpYXQiOjE3NTIwNzE5OTQsImp0aSI6ImQ2NGQ2NzU2M2JhZTRiZmE5ZjZlMzhmOGE3ZDM4ZDg4IiwidXNlcl9pZCI6NH0.MTL-7KqCYDpwUpvbSLiUx-PmKGbsbJID2KV_FQRDITs"
+    );
+  }, []);
   return (
     <Router>
       <TabsProvider>
